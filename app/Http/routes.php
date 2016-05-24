fb25beb814568eae937c115f9785a12bfa819441
oXg3n <a.perrier721@gmail.com>
2016-05-25 00:22:46 +0200

    some tracks to build a XML parser system...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -67,8 +67,64 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::get('/ftp', function () {
         $directoryName = 'ec_actu_tout_flux';
-        $fileName = 'ec_flux_actualites.xml';
-        $file = Storage::disk('ftp')->get($directoryName.'/'.$fileName);
-        Storage::disk('local')->put($fileName, $file);
+        $fileName = 'ec_flux_actualites';
+
+        // $file = Storage::disk('ftp')->get($directoryName.'/'.$fileName.'.xml');
+        // Storage::disk('local')->put($fileName.'.xml', $file);
+        
+        // $json = json_encode(Storage::disk('local')->get($fileName.'.xml'));
+
+        // Storage::disk('local')->put($fileName.'.json', $json);
+
+        $xml = XmlParser::load(storage_path('app/'.$fileName.'.xml'));
+
+        //bug d'affichage quand je display à la rache (en echo) : htmlentities fait l'affaire
+        //mais de toute façon on sera pas confronté au pb avec blade et les bdd
+
+        //$articles = simplexml_load_string(Storage::disk('local')->get($fileName.'.xml'));
+        $articles = $xml->getContent();
+        foreach ($articles as $article){
+            echo $article->id;
+            echo '<br>';
+            echo $article->create_date;
+            echo '<br>';
+            echo $article->display_date;
+            echo '<br>';
+            echo $article->author;
+            echo '<br>';
+            echo $article->language;
+            echo '<br>';
+            echo $article->title;
+            echo '<br>';
+            echo $article->summary;
+            echo '<br>';
+
+            echo $article->content->section->section_content->asXML();
+            
+            // foreach($article->content->section->section_content->children() as $item){
+            //     echo $item;
+            //     echo '<br>';
+            // }
+            
+            echo '<br>';
+
+            // foreach($article->content->section->section_content->annotation as $item){
+            //     echo $item->titreannotation;
+            //     echo '<br>';
+            //     echo $item; 
+            //     echo '<br>';
+            // }
+
+            // echo '<br>';
+            // echo $article->tag;
+            // echo '<br>';
+            // echo $article->media;
+            // echo '<br>';
+            // echo $article->copyright;
+            // echo '<br>';
+            // echo '<br>';
+            // echo '<br>';
+        }
+        dd($articles);
     });
 });
