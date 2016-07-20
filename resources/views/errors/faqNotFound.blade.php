f38947c96103a4cf0c1a9cc6e51e1add38ae484d
oXg3n <a.perrier721@gmail.com>
2016-07-20 19:56:39 +0200

    errors management improved (articles, partners, echosarticles, digitarticles, faq)

---
+++ resources/views/errors/faqNotFound.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('content')
+  <div class="row center-xs middle-xs not-found">
+    <div class="not-found__image">
+      @include('errors.picto')
+    </div>
+    <div class="not-found__body">
+      <h2>Oups !</h2>
+      <h3>La FAQ n'est pas disponible pour le moment.</h3>
+      <span><a href="{{URL::to('/')}}">Retour à l'accueil</a></span>
+    </div>
+  </div>
+@stop
---
+++ storage/framework/views/f1345c7ee1a85b425f02ab405959988750cf0f76.php
@@ -0,0 +1,14 @@
+<?php $__env->startSection('content'); ?>
+  <div class="row center-xs middle-xs not-found">
+    <div class="not-found__image">
+      <?php echo $__env->make('errors.picto', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    </div>
+    <div class="not-found__body">
+      <h2>Oups !</h2>
+      <h3>Aucun article n'est disponible pour le moment.</h3>
+      <span><a href="<?php echo e(URL::to('/')); ?>">Retour à l'accueil</a></span>
+    </div>
+  </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
