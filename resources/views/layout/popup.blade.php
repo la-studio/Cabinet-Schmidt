0f85b8ea430aaff7aaa622523cdc1f5706796cea
alainperrier <a.perrier721@gmail.com>
2019-07-19 18:19:23 +0200

    Legal updated

--- resources/views/layout/popup.blade.php
+++ resources/views/layout/popup.blade.php
@@ -1,98 +1,106 @@
 <div class="modal-bg"></div>
 <div class="modal-body">
-  <h2 class="modal-body__title">Mentions Légales</h2>
-  <h3 class="modal-body__label">Informations sur l'entreprise</h3>
-  <span class="modal-body__exit"><i class="material-icons">clear</i></span>
-  <div class="modal-body__main">
-    <div class="info">
-      <span class="info__label">Dénomination :</span>
-      <span class="info__desc">CABINET SCHMIDT</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Adresse :</span>
-      <span class="info__desc">81 Rue Henri Fabre 38920 CROLLES</span>
-    </div>
-    <div class="info">
-      <span class="info__label">SIREN :</span>
-      <span class="info__desc">451 393 508</span>
-    </div>
-    <div class="info">
-      <span class="info__label">SIRET (siege) :</span>
-      <span class="info__desc">45139350800010</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Capital social :</span>
-      <span class="info__desc">8 000 €</span>
-    </div>
-    <div class="info">
-      <span>Retrouvez toute l’information du cabinet Schmidt sur <a target="_blank" href="http://societe.com">societe.com</a></span>
-      <span>N’hésitez pas à nous contacter par email <a href="mailto:cabinet.pierre.schmidt@orange.fr">cabinet.pierre.schmidt&#64;orange.fr</a> ou par téléphone au 04 76 08 10 50</span>
-    </div>
-  </div>
-  <h3 class="modal-body__label">Hébergeur du site</h3>
-  <div class="modal-body__main">
-    <div class="info">
-      <span class="info__label">Nom de l’hébergeur :</span>
-      <span class="info__desc">OVH</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Raison sociale :</span>
-      <span class="info__desc">SAS au capital de 10 000 000 €</span>
-    </div>
-    <div class="info">
-      <span class="info__label">RCS :</span>
-      <span class="info__desc">Roubaix – Tourcoing 424 761 419 00045</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Code APE :</span>
-      <span class="info__desc">6202A</span>
-    </div>
-    <div class="info">
-      <span class="info__label">N° TVA :</span>
-      <span class="info__desc">FR 22 424 761 419</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Nom de l’hébergeur :</span>
-      <span class="info__desc">OVH</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Siège social :</span>
-      <span class="info__desc">2 rue Kellermann – 59100 Roubaix</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Téléphone :</span>
-      <span class="info__desc">0 820 698 765</span>
-    </div>
-    <div class="info">
-      <span class="info__label">Mail : </span>
-      <span class="info__desc">support@ovh.com</span>
-    </div>
-      <h3 class="modal-body__label">Responsable des actualités</h3>
-      <div class="modal-body__main">
-        <div class="info">
-          <span class="info__label">Editeur :</span>
-          <span class="info__desc">SAS Les Echos Publishing</span>
-        </div>
-        <div class="info">
-          <span class="info__label">Siège social :</span>
-          <span class="info__desc">16, rue du 4 Septembre 75002 Paris</span>
-        </div>
-        <div class="info">
-          <span class="info__label">Capital social :</span>
-          <span class="info__desc">1 728 750 euros</span>
-        </div>
-        <div class="info">
-          <span class="info__label">RCS :</span>
-          <span class="info__desc">381 123 868 Paris</span>
-        </div>
-        <div class="info">
-          <span class="info__label">Téléphone/Fax :</span>
-          <span class="info__desc">05 49 60 20 60/05 49 01 87 08</span>
-        </div>
-        <div class="info">
-          <span class="info__label">Production et administratif :</span>
-          <span class="info__desc">15, rue de la Demi-lune - BP 1119 86061 Poitiers Cedex 9</span>
-        </div>
-    </div>
+	<div class="info">
+	  <h2 class="modal-body__title">Mentions Légales</h2>
+	  <span>L'utilisateur est notamment informé, conformément à la loi du 6 janvier 1978 relative à l'Informatique, que les informations qu'il communique par le biais des formulaires présents sur le site sont nécessaires pour répondre à sa demande, et sont destinées au cabinet Pierre Schmidt, responsable du traitement. </span>
+	  <span>L'utilisateur est informé qu'il dispose d'un droit d'accès et de rectification portant sur les données le concernant auprès du cabinet Pierre SCHMIDT, 81 rue Henri Fabre 38920 CROLLES  ou par l'intermédiaire de notre adresse électronique : patrick.farina@rsmfrance.fr</span>
+	</div>	
+	<div class="modal-body__main">
+		<div class="info">
+			<span>Le Règlement général sur la protection des données est entré en vigueur le 25 mai 2018. Ces modifications concernent le recueil et le traitement des données personnelles. Ces changements s’appliquent à tous les utilisateurs depuis le 25 mai 2018.</span>
+			<h3 class="modal-body__label">Politique de protection des données personnelles</h3>
+			<span class="modal-body__exit"><i class="material-icons">clear</i></span>
+			<span>Le cabinet Pierre SCHMIDT s’engage à protéger vos données personnelles. La présente déclaration explique comment nous traitons vos informations personnelles collectées par notre site Internet.</span>
+			<span>La présente politique de protection des données personnelles s’applique donc à l’ensemble des sites internet, des applications, ainsi qu’à toute autre initiative en ligne et offline appartenant au cabinet Pierre SCHMIDT et procédant à la collecte de données personnelles.</span> 
+			<span>Par conséquent, la présente politique ne s’applique pas aux sites internet de tiers, y compris à ceux éventuellement mentionnés sur nos supports par l’intermédiaire d’un lien internet.</span>
+			<span>En naviguant sur ce site, vous acceptez les dispositions de cette déclaration de confidentialité, y compris le fait que le cabinet Pierre SCHMIDT procède, conformément à ces dernières, à la collecte, au traitement et à la transmission de vos données personnelles.</span>
+		</div>
+		<div class="info">
+			<ul>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Les informations recueillies</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Utilisation des cookies</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Utilisation de vos données personnelles</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Nos communications et nos activités promotionnelles</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">La divulgation des informations personnelles</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Stockage et de conservation des données</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Transfert international de vos données personnelles</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">La protection de votre information</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Liens vers des sites tiers</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Vos droits</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Les modifications de confidentialité</a></li>
+				<li><a target="_blank" href="https://www.rsm.global/france/fr/mise-jour-de-la-politique-de-confidentialite">Contactez-nous</a></li>
+			</ul>
+		</div>
+		<div class="info">
+			<h3 class="modal-body__label">Propriétés du site</h3>
+			<span>Le présent site est la propriété du cabinet Pierre SCHMIDT, société à responsabilité limitée d'Expertise Comptable et de Commissariat aux Comptes au capital de 8 000 euros inscrite au tableau de l'Ordre de la région Paris-Ile de France et membre de la Compagnie régionale de Paris.</span> 
+			<span>Siège social 81 rue Henri Fabre 38920 CROLLES, RCS  800 709 891 T 01 56 88 31 20.</span>
+			<span>Le responsable de la publication de ce site est la SAS Les Echos Publishing, siège social 16 rue du 4 septembre 75002 PARIS, capital social 1 728 750 Euros, RCS Paris 381 123 868, téléphone 05 49 60 20 60. Production et administration : 15 rue de la Demi-lune – BP 1119 86061 Poitiers Cedex 9.</span> 
+			<span>Le site est hébergé par la société OVH – SAS au capital de 10 000 000 € RCS Roubaix – Tourcoing 424 761 419 00045 ; siège social 2 rue Kellerman 59100 Roubaix, téléphone 0820 698 765, mail support@ovh.com</span>
+		</div>
+		
+		<div class="info">
+			<h3 class="modal-body__label">Droits et devoirs de l'utilisateur</h3>
+			<span>L'utilisateur du présent site reconnaît disposer de la compétence et des moyens nL'utilisateur est notamment informé, conformément à la loi du 6 janvier 1978 relative à l'Informatique, que les informations qu'il communique par le biais des formulaires présents sur le site sont nécessaires pour répondre à sa demande, et sont destinées au cabinet Pierre Schmidt, responsable du traitement. </span>
+			<span>L'utilisateur est informé qu'il dispose d'un droit d'accès et de rectification portant sur les données le concernant auprès du cabinet Pierre SCHMIDT, 81 rue Henri Fabre 38920 CROLLES  ou par l'intermédiaire de notre adresse électronique : patrick.farina@rsmfrance.fr</span>
+		</div>
+		
+		<div class="info">
+			<h3 class="modal-body__label">Utilisation des données du site</h3>
+			<span>Les utilisateurs du présent site sont tenus de respecter les dispositions de la loi relative à l'Informatique, aux fichiers et aux libertés, dont la violation est passible de sanctions pénales. </span>
+			<span>Ils doivent notamment s'abstenir, s'agissant des informations nominatives auxquelles ils accèdent, de toute collecte, de toute utilisation détournée, et d'une manière générale, de tout acte susceptible de porter atteinte à la vie privée ou à la réputation des personnes.</span>
+			<span>La structure générale, ainsi que les logiciels, textes, images (animées ou non), vidéos, son savoir-faire, et tous autres éléments composants le site sont la propriété exclusive du cabinet Pierre SCHMIDT.</span>
+			<span>Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l'autorisation expresse du cabinet Pierre SCHMIDT est interdite et constituerait une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle.</span>
+			<span>Il en est de même des bases de données figurant sur le site web, qui sont protégées par les dispositions de la loi du 1er juillet 1998 portant transposition dans le Code de la propriété intellectuelle de la directive européenne du 11 mars 1996 relative à la protection juridique des bases de données, et dont l'AFNIC est producteur.</span>
+			<span>La marque Cabinet SCHMIDT ainsi que les logos figurant sur le site sont des marques déposées ou sont la propriété de ses associés. Toute reproduction totale ou partielle de ces marques ou de ces logos, effectuée à partir des éléments du site sans l'autorisation expresse du cabinet Pierre SCHMIDT est donc prohibée, au sens de l'article L.713-2 du Code de la propriété intellectuelle. </span>
+		</div>
+
+		<div class="info">
+			<h3 class="modal-body__label">La reproduction sur support papier</h3>
+			<span>A l’exception de l'iconographie, la reproduction des pages de ce site sur un support papier est autorisée, sous réserve du respect des trois conditions suivantes :</span>
+			 <ul>
+				<li>gratuité de la diffusion,</li>
+				<li>respect de l'intégrité des documents reproduits (aucune modification, ni altération d'aucune sorte)</li>
+				<li>citation explicite du site https://www.cabinet-schmidt.com comme source et mention que les droits de reproduction sont réservés et strictement limités.</li>
+			</ul>
+		</div>	
+		
+		<div class="info">
+			<h3 class="modal-body__label">La reproduction sur support électronique</h3>
+			<span>La reproduction de tout ou partie de ce site sur un support électronique est autorisée sous réserve de l'ajout de façon claire et lisible de la source (https://www.cabinet-schmidt.com) et de la mention "Droits réservés". </span>
+			<span>Les informations utilisées ne doivent l'être qu'à des fins personnelles, associatives ou professionnelles ; toute utilisation à des fins commerciales ou publicitaires est exclue. </span>
+		</div>
+		
+		<div class="info">
+			<h3 class="modal-body__label">La création de liens vers www.cabinet-schmidt.com</h3>
+			<span>Le site www.cabinet-schmidt.com autorise la mise en place d’un lien hypertexte pointant vers son contenu, sous réserve de :</span>
+			 <ul>
+				<li>ne pas utiliser la technique du lien profond ("deep linking"), c'est-à-dire que les pages du site www.cabinet-schmidt.com ne doivent pas être imbriquées à l'intérieur des pages d'un autre site, mais accessible par l’ouverture d’une fenêtre.</li>
+				<li>mentionner la source qui pointera grâce à un lien hypertexte directement sur le contenu visé. Les informations utilisées ne doivent l'être qu'à des fins personnelles, associatives ou professionnelles ; toute utilisation à des fins commerciales ou publicitaires est exclue.</li>
+			</ul>
+			<span>Attention ! Cette autorisation ne s'applique pas aux sites internet diffusant des informations à caractère polémique, pornographique, xénophobe ou pouvant, dans une plus large mesure portée atteinte à la sensibilité du plus grand nombre. Pour d'autres utilisations, veuillez nous consulter. </span>
+		</div>
+		
+		<div class="info">
+			<h3 class="modal-body__label">Cookies</h3>
+			<span>Pour améliorer les performances de notre site et vous offrir une meilleure expérience de navigation, le site Internet rsmfrance.fr utilise des cookies.</span>
+			<span>Un « cookie » est un petit élément d'information que nous pouvons fournir à votre navigateur lorsque vous êtes sur notre site. Les cookies ne sont pas utilisés pour déterminer l'identité personnelle de tout individu visitant notre site Internet rsmfrance.fr. </span>
+			<span>Les cookies nous permettent d’effectuer un suivi des habitudes de consultation de notre site Internet afin de déterminer la localisation et la langue préférées de l'utilisateur et le diriger tout de suite sur la page d’accueil correcte lorsqu’il se rend sur notre site Internet rsmfrance.fr. </span>
+			<span>Sur certaines pages de notre site, les cookies sont utilisés pour nous aider à suivre vos centres d'intérêts et vous proposer les contenus les plus adaptés. </span>
+		</div>
+		
+		<div class="info">
+			<h3 class="modal-body__label">Comment puis-je désactiver les cookies?</h3>
+			<span>Si vous ne souhaitez pas recevoir de cookies de notre site Internet <a target="_blank" href="https://www.rsm.global/france/fr/">rsmfrance.fr</a> vous pouvez modifier les préférences de votre navigateur afin d'avoir la possibilité de les refuser ou d'en limiter l'acceptation. </span>
+		</div>
+		
+		<div class="info">
+			<h3 class="modal-body__label">Liens avec des tiers</h3>
+			<span>Nous attirons votre attention sur le fait que notre site Internet www.cabinet-schmidt.com peut contenir des liens vers d'autres sites. </span>
+			<span>Les visiteurs de notre site peuvent être redirigés vers d'autres sites Internet afin d’obtenir des compléments d’information concernant notamment nos événements et nos services. </span>
+			<span>Le Cabinet Pierre SCHMIDT ne peut en aucun cas être tenu pour responsable de la façon dont vos données seront stockées ou utilisées sur les serveurs de tiers. </span>
+			<span>Nous vous conseillons de prendre connaissance de la politique applicable en matière de protection des données personnelles de chaque site Internet tiers auquel vous accéderez via notre site Internet afin d’apprécier la manière dont seront utilisées vos données personnelles.</span>
+		</div>
+	
 </div>
 </div>
